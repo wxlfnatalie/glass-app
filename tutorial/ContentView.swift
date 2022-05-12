5758d4aca3a4f822724d46cc04b7a9d968510ec1
wxlfnatalie <76596210+wxlfnatalie@users.noreply.github.com>
2022-05-12 14:38:23 -0400

    Initial Commit

---
+++ tutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tutorial
+//
+//  Created by natalie wolf on 5/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
