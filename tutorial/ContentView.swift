2f6aa4c41705a4b4dc29a0dd71f21fe8d60b7aa3
wxlfnatalie <76596210+wxlfnatalie@users.noreply.github.com>
2022-05-12 21:29:12 -0400

    made ugly but first glassphormic tile. not pretty, have to find a color palette.

--- tutorial/ContentView.swift
+++ tutorial/ContentView.swift
@@ -9,13 +9,84 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        Home()
+
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+
+
+
+
+
+struct Home: View {
+    var body: some View {
+        
+        ZStack {
+            
+        Text("background here")
+        Color(.darkGray)
+        
+        VStack {
+            
+        Text("this is my first app")
+            
+            .fontWeight(.heavy)
+            .foregroundColor(.gray)
+            .italic()
+            .opacity(0.5)
+    
+            
+        Spacer()
+        
+        VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 10, content: {
+            Card()
+            
+        })
+            
+        Spacer()
+            
+        HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 10, content: {
+            
+            Button(action:{}) {Image("donut")}
+            
+            Button(action:{}) {Image("donut")}
+            
+            Button(action:{}) {Image("donut")}
+            
+            Button(action:{}) {Image("donut")}
+            
+            
+        })
+            
+        }
+        
+        
+            
+        }
     }
 }
+
+struct Card: View {
+    var body: some View {
+        let width = UIScreen.main.bounds.width
+        ZStack {
+            RoundedRectangle(cornerRadius: 25)
+                .fill(Color.white)
+                .opacity(0.1)
+                .background(
+                    Color.white
+                        .opacity(0.08)
+                        .blur(radius:10))
+                .background(
+                    RoundedRectangle(cornerRadius: 25)
+                        .stroke(LinearGradient(gradient: Gradient(colors:
+                                [.white,
+                                 .black]),
+                                startPoint: .top, endPoint: .bottom)))
+        .frame(width: width / 1.7, height: 270)}}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()}}}
